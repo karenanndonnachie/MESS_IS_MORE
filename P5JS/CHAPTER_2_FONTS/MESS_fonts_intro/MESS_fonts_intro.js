3020f7c94cc1f45b8bce7ba9c0ffba1091550f34
karenanndonnachie <karenanndonnachie@gmail.com>
2022-09-15 22:44:25 +1000

    Add files via upload

---
+++ P5JS/CHAPTER_2_FONTS/MESS_fonts_intro/MESS_fonts_intro.js
@@ -0,0 +1,31 @@
+var font1, font2, font3;
+
+function preload() {
+  font1 = loadFont('data/inconsolata.otf');
+  font2 = loadFont('data/DIN.otf');
+  font3 = loadFont('data/coldera.otf');
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(220);
+  fill(0);
+  textSize(32);
+  textFont(font1); //textFont(font,32);
+  textAlign(CENTER);
+}
+
+
+function draw() {
+  push();
+  textSize(32);
+  textFont(font1);
+  text ('inconsolata', 300,400);
+  pop();
+  textSize(60);
+  textFont(font2);
+  text ('DIN', 300,100);
+  textSize(32);
+  textFont(font3);
+  text ('COLDERA', 300,250);
+}
